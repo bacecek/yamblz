a01c14f8845517c325761c61deb8d911aba642f7
Denis Buzmakov <lysva94@gmail.com>
2017-07-07 22:15:43 +0300

    добавлен navigation drawer

--- app/src/main/java/com/bacecek/yamblz/MainActivity.java
+++ app/src/main/java/com/bacecek/yamblz/MainActivity.java
@@ -1,13 +1,72 @@
 package com.bacecek.yamblz;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar mToolbar;
+    @BindView(R.id.layout_drawer)
+    DrawerLayout mDrawerLayout;
+
+    private ActionBarDrawerToggle mActionBarToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        initToolbar();
+    }
+
+    /**
+     * Инициализация тулбара
+     */
+    private void initToolbar() {
+        setSupportActionBar(mToolbar);
+        setTitle(null);
+        mActionBarToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open, R.string.drawer_close);
+        mDrawerLayout.addDrawerListener(mActionBarToggle);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mActionBarToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mActionBarToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mActionBarToggle.onConfigurationChanged(newConfig);
     }
 }
